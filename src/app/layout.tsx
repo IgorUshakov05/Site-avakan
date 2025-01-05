b21bf6b5cc50e504b53d476f86c4a819b5765871
Igor <igorushakov111@gmail.com>
2025-01-05 19:01:55 +0700

    Начало seo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -38,7 +38,7 @@ const UnboundedSemiBold = localFont({
 
 export const metadata: Metadata = {
   title: "Site-Abakan",
-  description: "",
+  description: "WebHunt — студия разработки веб-приложений и сайтов.",
 };
 
 export default function RootLayout({
@@ -46,8 +46,42 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  // JSON-LD описание студии
+  const jsonLd = {
+    "@context": "https://schema.org",
+    "@type": "Organization",
+    name: "SiteAbakan",
+    url: "https://site-abakan.ru", // Укажите свой URL
+    logo: "https://site-abakan.ru/logo.png", // Укажите путь к вашему логотипу
+    sameAs: [
+      "https://twitter.com/WebHunt",
+      "https://facebook.com/WebHunt",
+      "https://instagram.com/WebHuntStudio",
+    ],
+    address: {
+      "@type": "PostalAddress",
+      addressLocality: "Абакан",
+      addressRegion: "Республика Хакасия",
+      postalCode: "655000",
+      addressCountry: "RU",
+    },
+    contactPoint: {
+      "@type": "ContactPoint",
+      telephone: "+7-123-456-7890",
+      contactType: "customer service",
+      areaServed: "RU",
+      availableLanguage: ["Russian", "English"],
+    },
+  };
+
   return (
     <html lang="ru">
+      <head>
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+        />
+      </head>
       <body
         className={`${UnboundedBlack.variable} ${UnboundedBold.variable} ${UnboundedExtraBold.variable} ${UnboundedExtraLight.variable} ${UnboundedLight.variable} ${UnboundedMedium.variable} ${UnboundedRegular.variable} ${UnboundedSemiBold.variable}`}
       >
@@ -56,4 +90,3 @@ export default function RootLayout({
     </html>
   );
 }
-
