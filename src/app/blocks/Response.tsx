9d8ffc4228e4da56ea37b64199c79e6947be8626
Igor <igorushakov111@gmail.com>
2025-01-14 18:39:13 +0700

    Рефактор

--- src/app/blocks/Response.tsx
+++ src/app/blocks/Response.tsx
@@ -2,21 +2,16 @@
 import ReviewItem from "./Review";
 import { useState, useEffect, useRef, useReducer } from "react";
 import style from "@/app/style/Response.module.css";
-
-interface Review {
-  client: string;
-  message: string[];
-}
+import { ResponseInfo } from "@/app/data/Response";
 
 function Response() {
-  let [reviews, setReviews] = useState<Review[]>([]);
   let elem = useRef<HTMLDivElement>(null);
   let [scrollPosition, setScrollPosition] = useState(0);
   let [stop, setStop] = useState(false);
   let [scrollDirection, setScrollDirection] = useState(1);
   enum ChangeSize {
-    X = "X",
-    Y = "Y",
+    X,
+    Y,
   }
 
   interface State {
@@ -73,47 +68,6 @@ function Response() {
       clearInterval(timer);
     };
   }, [scrollDirection, stop]);
-  useEffect(() => {
-    setReviews([
-      {
-        client: "Автошкола",
-        message: [
-          "С исполнителем очень легко было работать. Все замечания и предложения учитывал. Предлагал свои идеи, большинство из которых мы реализовали. Сайт получился как задумывалось. Выполнен был раньше срока на 4 дня, что позволило ускорить работу над версткой. Полностью довольна, надеюсь на дальнейшее сотрудничество по развитию сайта)",
-        ],
-      },
-      {
-        client: "Строительство",
-        message: [
-          "Отличный дизайнер с ответственным подходом!",
-          "Все отлично, не первый раз обращаюсь к данному специалисту, выполняет работу качественно и раньше срока",
-        ],
-      },
-      {
-        client: "Юрисдикция",
-        message: [
-          "Очень нравится оперативность и качество работы. Дизайнер слышит все пожелания и воплощает их в работе",
-        ],
-      },
-      {
-        client: "Строительство",
-        message: [
-          "Пизд#то, знаю что не наша верстка, но дизайн твой и он охуе#ный",
-        ],
-      },
-      {
-        client: "Финансы",
-        message: [
-          "Доброе утро, чекнул щас то что у тебя выходит, белая тема тоже секс просто, цвета прям и стиль подобраны идеально",
-        ],
-      },
-      {
-        client: "Вы",
-        message: [
-          "Можете сотрудничать с нами  и оказаться здесь, написав нам отзыв",
-        ],
-      },
-    ]);
-  }, []);
   function stopScroll(): void {
     setStop(true);
   }
@@ -131,7 +85,7 @@ function Response() {
             onMouseEnter={stopScroll}
             onMouseLeave={startScroll}
           >
-            {reviews.map((item, index) => {
+            {ResponseInfo.map((item, index) => {
               return (
                 <ReviewItem
                   key={index}
